2a47c7494b9d8bd064d34c688b71250dd646aab4
Lewis Weaver <lweaver@deloitte.com.au>
2016-09-01 17:04:36 +1000

    fixed error where processing multiple files would exit early

--- tasks/apiblueprint-springmvc.js
+++ tasks/apiblueprint-springmvc.js
@@ -10,7 +10,8 @@
 
 module.exports = function (grunt) {
 
-  var generator = require('apiblueprint-springmvc');
+  var generator = require('apiblueprint-springmvc'),
+    Q = require('q');
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
@@ -27,37 +28,56 @@ module.exports = function (grunt) {
       packagePrefix: null
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function (file) {
-      // Concat specified files.
-      var filepaths = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function (filepath) {
-        // Read file source.
-        return filepath;
-      });
+    try {
+      execute(this.files, options, done);
+    } catch (ex) {
+      grunt.log.error(ex);
+      done(false);
+    }
 
-      // Process files
-      filepaths.forEach(function(filepath) {
-        grunt.log.writeln('parsing file "' + filepath + '"');
-        generator.render(filepath, file.dest + '', options, function(err) {
-          if (err) {
-            grunt.log.error(err.message || err);
-            done(false);
+    function execute(files, options, done) {
+      // Iterate over all specified file groups.
+      var funcs = [];
+      files.forEach(function (file) {
+        // Concat specified files.
+        var filepaths = file.src.filter(function (filepath) {
+          // Warn on and remove invalid source files (if nonull was set).
+          if (!grunt.file.exists(filepath)) {
+            grunt.log.warn('Source file "' + filepath + '" not found.');
+            return false;
           } else {
-            // Print a success message.
-            grunt.log.writeln('Files created in "' + file.dest + '".');
-            done(true);
+            return true;
           }
+        }).map(function (filepath) {
+          // Read file source.
+          return filepath;
+        });
+
+        // Process files
+        filepaths.forEach(function (filepath) {
+          funcs.push(function() {
+            //noinspection JSCheckFunctionSignatures
+            var deferred = Q.defer();
+            grunt.log.writeln('parsing file "' + filepath + '" into "'+file.dest+'"');
+            generator.render(filepath, file.dest + '', options, function(err) {
+              if (err)
+                deferred.reject(err.message || err);
+              else
+                deferred.resolve();
+            });
+            return deferred.promise;
+          });
         });
       });
-    });
-  });
 
+      funcs.reduce(Q.when, Q()).then(
+        function() {
+          done(true);
+        }, function(err) {
+          grunt.log.error(err);
+          done(false);
+        }
+      );
+    }
+  });
 };
